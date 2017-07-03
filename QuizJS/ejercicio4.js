fbddfcc0699709e1cfee0fb6aca5911850865dc0
luisaGonzales <luisagc711@gmail.com>
2017-07-03 17:00:59 -0500

    Con ejercicio 4

--- QuizJS/ejercicio4.js
+++ QuizJS/ejercicio4.js
@@ -1,17 +1,18 @@
-var n = 4;
-function espacios(n){
+var n = parseInt(prompt("Ingrese el número de rombos"));
+
+function espacios (n)
+{
   var s = "";
-  for (var i = 0; i < n; i++){
-    s += " ";
-  }
+  for (var i = 0; i < n; i++)
+   s += "*";
   return s;
 }
 
-for (var i = 0; i < n; i++){
-  var sp = espacios(n-(i+1));
-  var num = "";
-  for (var j = 0; j < i + 1; j++){
-    num += j;
-  }
+for (var k = 0; k < 2 * n - 1; k++) {
+  var i = k < n ? k :  2*n - 2 - k;
+  var content = "";
+  for (j = 1; j <= 2*i+1; j++)
+    content +=  j;
+  var sp = espacios (n - (i+1));
+  document.write(sp + content + "</br>");
 }
-console.log(sp + num);
